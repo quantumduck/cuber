2a6665d59a18df8b7f2795f43b74f1c6a0b460a0
quantumduck <quantum.duck@hotmail.com>
2017-06-05 16:59:27 -0400

    partial completion of drawing method

--- js/model.js
+++ js/model.js
@@ -35,14 +35,38 @@ function newPoint(x, y, z) {
     },
     projection: function() {
       return {
-        x: -2 * this.y + this.x + this.z,
-        y: this.z - this.x
+        row: this.x + this.z - (2 * this.y),
+        col: this.x - this.z
       }
     }
   }
 }
 
-function drawCollection(collection){
+function drawCollection(collection) {
   var offset = [0,0];
   var drawing = [[]];
+  for (var k = 0; k < collection.length; k++) {
+    var nextPoint = projection(collection[k]);
+    if (offset[0] + nextPoint.row < 0) {
+      for (var j = 0; j < drawing.length; j++) {
+        for (var i = 0; i <= offset[0] - nextPoint.row + 2) {
+          drawing[0].unshift(' ');
+
+        }
+      }
+      offset[0] -= nextPoint.row;
+    }
+    if (offset[1] + nextPoint.col < 0) {
+      var blankRow = [];
+      for (var i = 0; i < drawing[0].length; i++) {
+        blankRow.push(' ');
+      }
+      for (var j = 0; j <= offset[1] - nextPoint.col; j++) {
+        drawing.unshift(blankRow);
+      }
+    }
+    if (nextPoint.row >= drawing[0].length - offset[0]) {
+      
+    }
+  }
 }

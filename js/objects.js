7712790d7539de278a1d089f085214efa9a8c539
quantumduck <quantum.duck@hotmail.com>
2017-06-06 19:32:12 -0400

    definitions done

--- js/objects.js
+++ js/objects.js
@@ -1,10 +1,79 @@
-function newITet(x, y, z, orientation) {
-  var type = "I-tetronimo";
+function newLongPiece(x, y, z, orientation) {
+  var type = "long-piece";
   return newCollection([
     newCube(x, y, z, type),
     newCube(x, y, z + 1, type),
     newCube(x, y, z + 2, type),
     newCube(x, y, z + 3, type)
   ]);
+}
 
+function newLPiece(x, y, z, orientation) {
+  var type = "L-piece";
+  return newCollection([
+    newCube(x, y, z, type),
+    newCube(x, y, z + 1, type),
+    newCube(x, y, z + 2, type),
+    newCube(x + 1, y, z, type)
+  ]);
+}
+
+function newSquarePiece(x, y, z, orientation) {
+  var type = "square-piece";
+  return newCollection([
+    newCube(x, y, z, type),
+    newCube(x, y, z + 1, type),
+    newCube(x + 1, y, z, type),
+    newCube(x + 1, y, z + 1, type)
+  ]);
+}
+
+function newTPiece(x, y, z, orientation) {
+  var type = "T-piece";
+  return newCollection([
+    newCube(x, y, z, type),
+    newCube(x, y, z + 1, type),
+    newCube(x + 1, y, z, type),
+    newCube(x - 1, y, z, type)
+  ]);
+}
+
+function newSPiece(x, y, z, orientation) {
+  var type = "S-piece";
+  return newCollection([
+    newCube(x, y, z, type),
+    newCube(x, y, z + 1, type),
+    newCube(x + 1, y, z + 1, type),
+    newCube(x + 1, y, z + 2, type)
+  ]);
+}
+
+function newTripodPiece(x, y, z, orientation) {
+  var type = "tripod-piece";
+  return newCollection([
+    newCube(x, y, z, type),
+    newCube(x, y, z + 1, type),
+    newCube(x + 1, y, z, type),
+    newCube(x, y + 1, z, type)
+  ]);
+}
+
+function newWeirdPiece1(x, y, z, orientation) {
+  var type = "weird-piece-1";
+  return newCollection([
+    newCube(x, y, z, type),
+    newCube(x, y, z + 1, type),
+    newCube(x + 1, y, z, type),
+    newCube(x + 1, y + 1, z, type)
+  ]);
+}
+
+function newWeirdPiece2(x, y, z, orientation) {
+  var type = "weird-piece-2";
+  return newCollection([
+    newCube(x, y, z, type),
+    newCube(x, y, z + 1, type),
+    newCube(x + 1, y, z, type),
+    newCube(x + 1, y - 1, z, type)
+  ]);
 }

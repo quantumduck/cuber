ddc5412204aef9ec564a953635ef52f1496459f8
quantumduck <quantum.duck@hotmail.com>
2017-06-07 09:53:42 -0400

    fixing bug with colour setting

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-window.cursorCube = newCube(0,0,0);
+window.type = Math.random() * 3;
+if (window.type > 2) {
+  window.type = 'type3';
+} else if (window.type > 1) {
+  window.type = 'type2';
+} else {
+  window.type = 'type1';
+}
+window.cursorCube = newCube(0,0,0, window.type);
 window.allCubes = newCollection([window.cursorCube]);
 
 $(function() {
@@ -10,22 +18,24 @@ $(function() {
   $(window).on('keydown', function(e) {
     var cubesToDraw = newCollection(window.allCubes);
     var type = Math.random() * 3;
-    if (type > 2) {
-      type = 'type3';
-    } else if (type > 1) {
-      type = 'type2';
-    } else {
-      type = 'type1';
-    }
+
     // Parsing the keyboard
     // window.allCubes.deleteCube(window.cursorCube);
     switch (e.key) {
       case 's':
-        window.allCubes.addCube(newCube(window.cursorCube.x, window.cursorCube.y, window.cursorCube.z, type));
+        window.allCubes.addCube(newCube(window.cursorCube.x, window.cursorCube.y, window.cursorCube.z, window.type));
         console.log('(' + window.cursorCube.x + ', ' + window.cursorCube.y + ', ' + window.cursorCube.z + ')');
         console.log('(' + window.cursorCube.projection().col + ', ' +  window.cursorCube.projection().row + ')');
         cubesToDraw.addCube(window.cursorCube);
-        window.cursorCube = newCube(0,0,0);
+        window.type = Math.random() * 3;
+        if (window.type > 2) {
+          window.type = 'type3';
+        } else if (window.type > 1) {
+          window.type = 'type2';
+        } else {
+          window.type = 'type1';
+        }
+        window.cursorCube = newCube(0,0,0, window.type);
         break;
       case 'a':
         window.cursorCube.y--;

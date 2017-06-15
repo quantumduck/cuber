904fff26f45baa81e3fe6f250d27319a84f6107d
quantumduck <quantum.duck@hotmail.com>
2017-06-15 14:36:09 -0400

    changing background and making active cubes show up at start

--- js/main.js
+++ js/main.js
@@ -3,19 +3,21 @@ window.allCubes = newCollection([]); // Collection of all cube objects to be
                                     // displayed.
 // Adding background cubes:
 for (var i = 0; i < 10; i++) {
-  window.allCubes.addCube(newCube(-1,i,-1,'bg'));
-  window.allCubes.addCube(newCube(i,10,-1,'bg'));
-  window.allCubes.addCube(newCube(-1,10,i,'bg'));
+  // window.allCubes.addCube(newCube(-1,i,-1,'bg'));
+  // window.allCubes.addCube(newCube(i,10,-1,'bg'));
+  // window.allCubes.addCube(newCube(-1,10,i,'bg'));
   for (var j = 0; j < 10; j++) {
     window.allCubes.addCube(newCube(i,j,-1,'bg'));
-    window.allCubes.addCube(newCube(-1,i,j,'bg'));
-    window.allCubes.addCube(newCube(i,10,j,'bg'));
+    // window.allCubes.addCube(newCube(-1,i,j,'bg'));
+    // window.allCubes.addCube(newCube(i,10,j,'bg'));
   }
 }
 window.rotateMode = false; // flag for turning on/off rotations
 
 $(function() {
-  $('#drawing-area').html(drawCollection(window.allCubes));
+  var startingCubes = newCollection(window.allCubes);
+  startingCubes.addCubes(window.activeTet);
+  $('#drawing-area').html(drawCollection(startingCubes));
 
   $(window).on('keydown', function(e) {
     console.log(e.key);
